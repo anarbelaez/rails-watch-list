d63906df279c158604ff8afe6d3d66d4ba6fbf61
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-24 23:09:48 -0500

    Added margin top to general container. I have modify some small items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home
     @lists = List.last(5)
     @bookmarks = Bookmark.last(11)
-    @movies = Movie.first(4)
+    @movies = Movie.first(7)
   end
 end

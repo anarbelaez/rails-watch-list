0a37868b94b1f99f0b35d5d7fabeb0e87312af65
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-24 20:22:21 -0500

    Added a ghost card

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 require 'faker'
 
-# movies = {
-#   'My Neighbour Totoro' => 'https://picfiles.alphacoders.com/355/thumb-355178.jpg',
-#   'Ponyo' => 'https://picfiles.alphacoders.com/349/thumb-349135.jpg',
-#   'Spirited Away' => 'https://picfiles.alphacoders.com/350/thumb-350207.jpg',
-#   'Nausicaä of the Valley of the Wind' => 'https://picfiles.alphacoders.com/356/thumb-356316.jpg',
-#   'Castle in the Sky' => 'https://picfiles.alphacoders.com/800/thumb-80073.jpg'
-# }
+movies = {
+  'My Neighbour Toto' => 'https://picfiles.alphacoders.com/355/thumb-355178.jpg',
+  'Pony' => 'https://picfiles.alphacoders.com/349/thumb-349135.jpg',
+  'Spirited Aw' => 'https://picfiles.alphacoders.com/350/thumb-350207.jpg',
+  'Nausicaä of the Valley of the Win' => 'https://picfiles.alphacoders.com/356/thumb-356316.jpg',
+  'Castle in the Sk' => 'https://picfiles.alphacoders.com/800/thumb-80073.jpg'
+}
 
-# movies.each do |key, value|
-#   movie = Movie.create(
-#     title: key,
-#     overview: Faker::JapaneseMedia::StudioGhibli.quote,
-#     poster_url: value,
-#     rating: rand(8.0..10.0).floor(1)
-#   )
-#   movie.save!
-# end
-
-lists = %w[Japanese Chinese Historical]
-
-lists.each do |list|
-  List.create(
-    name: list
+movies.each do |key, value|
+  movie = Movie.create(
+    title: key,
+    overview: Faker::JapaneseMedia::StudioGhibli.quote,
+    poster_url: value,
+    rating: rand(8.0..10.0).floor(1)
   )
+  movie.save!
 end
+
+# lists = %w[Japanese Chinese Historical]
+
+# lists.each do |list|
+#   List.create(
+#     name: list
+#   )
+# end
